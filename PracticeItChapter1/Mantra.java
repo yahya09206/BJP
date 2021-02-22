8a7980d6105bb9a29ca6e9ea50674b7d0a13d14a
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-21 21:19:53 -0800

    deleted previous exercises

--- PracticeItChapter1/Mantra.java
+++ PracticeItChapter1/Mantra.java
@@ -1,21 +1,9 @@
-/*
-Write a complete Java program in a class named Mantra that produces the following output. Remove its redundancy by adding a method.
-
-There's one thing every coder must understand:
-The System.out.println command.
-
-There's one thing every coder must understand:
-The System.out.println command.
-*/
-public class Mantra {
-    public static void main(String[] args){
-        mantra();
-        System.out.println();
-        mantra();
-    }
-    
-    public static void mantra(){
-        System.out.println("There's one thing every coder must understand:");
-        System.out.println("The System.out.println command.");
-    }
+public class Mantra {
+	public static void main(String args[]){
+		System.out.println("\\\\");
+System.out.println("'");
+System.out.println("\ta\tb\tc");
+System.out.println("\"\"\"");
+System.out.println("C:\nin\the downward spiral");
+	}
 }
