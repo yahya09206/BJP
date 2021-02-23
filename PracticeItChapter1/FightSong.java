b03591fc4eccec3451122f1b6a16468539329133
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-22 20:46:36 -0800

    done with fight songs

---
+++ PracticeItChapter1/FightSong.java
@@ -0,0 +1,23 @@
+public class FightSong {
+	public static void main(String[] args){
+		first();
+		System.out.println();
+		first();
+		second();
+		first();
+		System.out.println();
+		first();
+		second();
+		first();
+		System.out.println();
+		first();
+	}
+	public static void first() {
+		System.out.println("Go, team, go!");
+		System.out.println("You can do it.");
+	}
+	public static void second() {
+		System.out.println("You're the best,");
+		System.out.println("In the West.");
+	}
+}
