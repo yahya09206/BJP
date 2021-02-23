ca80c9cfacdd4ee21c99183cc65a91a3a32b25d0
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-22 20:19:53 -0800

    re did stewie 2

---
+++ PracticeItChapter1/Stewie2.java
@@ -0,0 +1,20 @@
+public class Stewie2 {
+	public static void main(String[] args){
+		full();
+		half();
+		half();
+		half();
+		half();	
+	}
+
+	public static void full(){
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+
+	public static void half(){
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+}
