dccdec59df48a3e3e772491825779ee9a3de1cc1
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-27 17:55:28 -0800

    done with part 1

---
+++ PracticeItChapter2/NumbersOutputSixty.java
@@ -0,0 +1,11 @@
+public class NumbersOutputSixty {
+	public static void main(String[] args){
+		//For loop for bar
+		for (int i = 0; i < 6; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.print(" ");
+			}
+			System.out.print("|");
+		}
+	}
+}
