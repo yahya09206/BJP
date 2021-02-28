e6f80dc7e29dea01494eac221bf578ad6815fc45
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-27 17:08:01 -0800

    done with spacednumbers

---
+++ PracticeItChapter2/SpacedNumbers.java
@@ -0,0 +1,10 @@
+public class SpacedNumbers {
+	public static void main(String[] args){
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 5 - i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			System.out.println(i);
+		}
+	}
+}
