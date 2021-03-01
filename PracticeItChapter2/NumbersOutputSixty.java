2eb18e836d964604e744cdb7ca8497759984e660
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-28 20:59:33 -0800

    done with numbersoutput

--- PracticeItChapter2/NumbersOutputSixty.java
+++ PracticeItChapter2/NumbersOutputSixty.java
@@ -7,5 +7,12 @@ public class NumbersOutputSixty {
 			}
 			System.out.print("|");
 		}
+		System.out.println();
+		//For loop for numbers
+		for (int i = 0; i < 6; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.print(j%10);		
+			}	
+		}
 	}
 }
