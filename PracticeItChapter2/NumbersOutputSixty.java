6f986d6fa5be9777b55d0453e3fd9e69c486ef51
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-01 16:15:09 -0800

    done with numbersoutput

--- PracticeItChapter2/NumbersOutputSixty.java
+++ PracticeItChapter2/NumbersOutputSixty.java
@@ -1,3 +1,9 @@
+/*
+It's common to print a rotating, increasing list of single-digit numbers at the start of a program's output as a visual guide to number the columns of the output to follow. With this in mind, write nested for loops to produce the following output, with each line 60 characters wide:
+
+         |         |         |         |         |         |
+123456789012345678901234567890123456789012345678901234567890
+*/
 public class NumbersOutputSixty {
 	public static void main(String[] args){
 		//For loop for bar
