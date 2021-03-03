e62179a103e07e5fb2a7810d868b1ed384780b51
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-02 21:42:12 -0800

    done with largestabsvalue

---
+++ PracticeItChapter3/LargestAbsValue.java
@@ -0,0 +1,12 @@
+/*
+Write a method largestAbsVal that accepts three integers as parameters and returns the largest of their three absolute values. For example, a call of largestAbsVal(7, -2, -11) would return 11, and a call of largestAbsVal(-4, 5, 2) would return 5.
+*/
+public class LargestAbsValue {
+	public static void main(String[] args){
+		System.out.println(largestAbsVal(7, -2, -11));
+		System.out.println(largestAbsVal(-4, 5, 2));
+	}
+	public static int largestAbsVal(int a, int b, int c){
+		return Math.max(Math.abs(a),Math.max(Math.abs(b), Math.abs(c)));
+	}
+}
