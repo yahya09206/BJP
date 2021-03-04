edbe3784f189fbb5a4ab8a0a4ffc9a21ee4b7b60
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-03 17:07:10 -0800

    done with spherevolume

---
+++ PracticeItChapter3/SphereVolume.java
@@ -0,0 +1,13 @@
+/*
+Write a method called sphereVolume that accepts a radius (a real number) as a parameter and returns the volume of a sphere with that radius. For example, the call sphereVolume(2.0) should return 33.510321638291124. The formula for the volume of a sphere with radius r is the following:
+
+volume = 4/3 π r3
+*/
+public class SphereVolume {
+	public static void main(String[] args){
+		System.out.println(sphereVolume(2.0));
+	}
+	public static double sphereVolume(double r){
+		return 4 * Math.PI * r * r * r / 3;
+	}
+}
