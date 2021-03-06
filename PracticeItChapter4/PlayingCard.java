1dd1a53cbb1a244ae23c07deaf89fce5a26d15b9
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-05 23:09:20 -0800

    set up template for file

---
+++ PracticeItChapter4/PlayingCard.java
@@ -0,0 +1,16 @@
+/*
+Write a piece of code that reads a shorthand text description of a playing card and prints the longhand equivalent. The shorthand description is the card's rank (2 through 10, J, Q, K, or A) followed by its suit (C, D, H, or S). You should expand the shorthand into the form "Rank of Suit". You may assume that the user types valid input.
+
+Here are two sample executions:
+
+Enter a card: 9 S
+Nine of Spades
+Enter a card: K C
+King of Clubs
+
+*/
+public class PlayingCard {
+	public static void main(String[] args){
+		
+	}
+}
