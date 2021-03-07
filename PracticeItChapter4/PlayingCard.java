92c5faa218b77cc43a1530886085bbcd82cf03ff
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-06 18:48:12 -0800

    done with playing cards

--- PracticeItChapter4/PlayingCard.java
+++ PracticeItChapter4/PlayingCard.java
@@ -9,8 +9,52 @@ Enter a card: K C
 King of Clubs
 
 */
+import java.util.*;
 public class PlayingCard {
 	public static void main(String[] args){
-		
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter a card: ");
+		String rank = console.next();
+		String suit = console.next();
+
+		if (rank.equals("2")) {
+			rank = "Two";
+		}else if (rank.equals("3")) {
+			rank = "Three";
+		}else if (rank.equals("4")) {
+			rank = "Four";
+		}else if (rank.equals("5")) {
+			rank = "Five";
+		}else if (rank.equals("6")) {
+			rank = "Six";
+		}else if (rank.equals("7")) {
+			rank = "Seven";
+		}else if (rank.equals("8")) {
+			rank = "Eight";
+		}else if (rank.equals("9")) {
+			rank = "Nine";
+		}else if (rank.equals("10")) {
+			rank = "Ten";
+		}else if (rank.equalsIgnoreCase("J")) {
+			rank = "Jack";
+		}else if (rank.equalsIgnoreCase("Q")) {
+			rank = "Queen";
+		}else if (rank.equalsIgnoreCase("K")) {
+			rank = "King";
+		}else {
+			rank = "Ace";
+		}
+
+		if (suit.equalsIgnoreCase("C")) {
+			suit = "Clubs";
+		}else if(suit.equalsIgnoreCase("D")){
+			suit = "Diamonds";
+		}else if(suit.equalsIgnoreCase("H")){
+			suit = "Hearts";
+		}else {
+			suit = "Spades";
+		}
+
+		System.out.print(rank + " of " + suit);
 	}
 }
