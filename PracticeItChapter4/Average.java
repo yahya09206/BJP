98da513e5f50494b8da1776ef76e9eb9d2ae8cf6
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-08 21:07:49 -0800

    done with avergae

---
+++ PracticeItChapter4/Average.java
@@ -0,0 +1,12 @@
+/*
+Write a method called average that takes two integers as parameters and returns the average of the two integers.
+*/
+public class Average {
+	public static void main(String[] args){
+		average();
+	}
+	public static double average(int a, int b){
+		double avg = a + b;
+		return avg / 2;
+	}
+}
