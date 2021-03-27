662b2c40cf72ac42da354136edc3b9942c3cee79
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-26 18:11:40 -0700

    added description

---
+++ PracticeItChapter7/OddsArray.java
@@ -0,0 +1,3 @@
+/*
+Write code that creates an array named odds and stores all odd numbers between -6 and 38 into it using a for loop. Make the array's size exactly large enough to store the numbers.
+*/
