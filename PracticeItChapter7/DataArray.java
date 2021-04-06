d060a985fcc233a3b1b32688351ddb1bbab37922
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-05 20:51:35 -0700

    done with dataArray exercise

--- PracticeItChapter7/DataArray.java
+++ PracticeItChapter7/DataArray.java
@@ -4,3 +4,9 @@ Write code that creates an array of integers named data of size 5 with the follo
 [27, 51, 33, -1, 101]
 
 */
+import java.util.*;
+public class DataArray {
+	public static void main(String[] args){
+		int[] data = {27, 51, 33, -1, 101};
+	}
+}
