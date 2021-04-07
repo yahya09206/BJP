2f3f6d45c2134ab25ec465292a8b9109733db63e
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-06 22:03:30 -0700

    working max array

--- PracticeItChapter7/Max.java
+++ PracticeItChapter7/Max.java
@@ -1,3 +1,12 @@
 /*
 Write a method called max that accepts an array of integers as a parameter and returns the maximum value in the array. For example, if the array passed stores {12, 7, -1, 25, 3, 9}, your method should return 25. You may assume that the array contains at least one element. Your method should not modify the elements of the array.
 */
+import java.util.*;
+public class Max {
+	public static void main(String[] args){
+
+	}
+	public static void int max(int[] num){
+		
+	}
+}
