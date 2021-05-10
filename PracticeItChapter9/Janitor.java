005243f96e24da6e75ead638970d0bfcafc98d23
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-09 19:04:51 -0700

    method for janitor vacation days

--- PracticeItChapter9/Janitor.java
+++ PracticeItChapter9/Janitor.java
@@ -11,6 +11,10 @@ public class Janitor extends Employee {
 		return super.getSalary() - 10000.00;
 	}
 
+	public int getVacationDays(){
+		return super.getVacationDays() / 2;
+	}
+
 	public void clean(){
 		System.out.println("Workin' for the man.");
 	}
