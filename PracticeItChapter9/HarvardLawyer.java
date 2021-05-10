7bb810fd89dffe0d45cef33db61ea0b6ce57aa04
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-09 19:08:11 -0700

    get salary and vacation methods to harvard lawyer class

--- PracticeItChapter9/HarvardLawyer.java
+++ PracticeItChapter9/HarvardLawyer.java
@@ -2,5 +2,13 @@
 Write a class HarvardLawyer to accompany the other law firm classes described in this chapter. Harvard lawyers are like normal lawyers, but they make 20% more money than a normal lawyer, they get 3 days more vacation, and they have to fill out four of the lawyer's forms to go on vacation. That is, the getVacationForm method should return "pinkpinkpinkpink". Make sure to interact with the superclass as appropriate.
 */
 public class HarvardLawyer extends Employee {
-	
+
+
+	public double getSalary(){
+		return super.getSalary() * .20;
+	}
+
+	public int getVacationDays(){
+		return super.getVacationDays() + 3;
+	}
 }
