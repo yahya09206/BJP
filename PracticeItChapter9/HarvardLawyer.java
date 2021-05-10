4053851b42bb1dcc6fea5ceef2bbea9006cfc20e
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-09 19:06:27 -0700

    new class for harvard lawyer

---
+++ PracticeItChapter9/HarvardLawyer.java
@@ -0,0 +1,3 @@
+/*
+Write a class HarvardLawyer to accompany the other law firm classes described in this chapter. Harvard lawyers are like normal lawyers, but they make 20% more money than a normal lawyer, they get 3 days more vacation, and they have to fill out four of the lawyer's forms to go on vacation. That is, the getVacationForm method should return "pinkpinkpinkpink". Make sure to interact with the superclass as appropriate.
+*/
