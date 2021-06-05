e75b7189536ebe3e313743490c52ec9755aa8217
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-04 23:43:49 -0700

    done with spikey

--- PracticeItChapter1/Spikey.java
+++ PracticeItChapter1/Spikey.java
@@ -9,11 +9,11 @@ Write a complete Java program in a class named Spikey that prints the following 
 */
 public class Spikey {
 	public static void main(String[] args){
-		System.out.println("\\/");
-		System.out.println("\\\\//");
+		System.out.println("  \\/");
+		System.out.println(" \\\\//");
 		System.out.println("\\\\\\///");
 		System.out.println("///\\\\\\");
-		System.out.println("//\\\\");
-		System.out.println("/\\");
+		System.out.println(" //\\\\");
+		System.out.println("  /\\");
 	}
 }
