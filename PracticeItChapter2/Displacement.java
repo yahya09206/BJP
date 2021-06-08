968fd06d7f226a959db333e86349cecdd226e63f
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-08 12:46:23 -0700

    re doing of chapter 2 started

--- PracticeItChapter2/Displacement.java
+++
@@ -1,9 +0,0 @@
-public class Displacement {
-	public static void main(String[] args){
-		double position = 12.0;
-		double velocity = 3.5 * 10;
-		double time = .5 * 9.8 * 10 * 10;
-		double total = position + velocity + time;
-		System.out.println(total);
-	}
-}
