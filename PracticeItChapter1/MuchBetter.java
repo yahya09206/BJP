03061825cb2da087b94a679d338646ffde338dd3
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-07 19:37:26 -0700

    done with much better exercise

---
+++ PracticeItChapter1/MuchBetter.java
@@ -0,0 +1,15 @@
+/*
+Write a complete Java program in a class named MuchBetter that prints the following output:
+
+A "quoted" String is
+'much' better if you learn
+the rules of "escape sequences."
+Also, "" represents an empty String.
+Don't forget: use \" instead of " !
+'' is not the same as "
+*/
+public class MuchBetter {
+	public static void main(String[] args){
+		System.out.println("A \"quoted\" String is\n'much' better if you learn \nthe rules of \"escape sequences.\" \nAlso, \"\" represents an empty String. \nDon't forget: use \\\" instead of \" ! \n'' is not the same as \"");
+	}
+}
