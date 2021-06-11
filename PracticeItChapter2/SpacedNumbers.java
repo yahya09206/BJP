c4587902fc4b1aa7840b206a10061008aec0b35f
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-10 20:09:11 -0700

    done with spacednumbers

---
+++ PracticeItChapter2/SpacedNumbers.java
@@ -0,0 +1,19 @@
+/*
+Write nested for loops to produce the following output:
+
+    1
+   2
+  3
+ 4
+5
+*/
+public class SpacedNumbers {
+	public static void main(String[] args){
+		for(int i = 1; i <= 5; i++) {
+    		for(int j = 5 - i; j > 0; j--) {
+        	System.out.print(" ");
+    		}
+    	System.out.println(i);
+		}
+	}
+}
