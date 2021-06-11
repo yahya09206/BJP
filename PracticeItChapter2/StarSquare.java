d205a8372078c35027e4ecb3880d8bfc854fd5a7
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-10 19:52:51 -0700

    done with star square

---
+++ PracticeItChapter2/StarSquare.java
@@ -0,0 +1,17 @@
+/*
+Write for loops to produce the following output:
+*****
+*****
+*****
+*****
+*/
+public class StarSquare {
+	public static void main(String[] args){
+		for(int i = 1; i <= 4; i++){
+			for(int j = 1; j <= 5; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
