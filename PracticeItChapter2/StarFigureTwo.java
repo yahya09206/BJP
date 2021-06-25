20ca9b2abc670759df5c27ad8d980e90b8d92572
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-24 20:57:42 -0700

    done with star figure two with constant

--- PracticeItChapter2/StarFigureTwo.java
+++ PracticeItChapter2/StarFigureTwo.java
@@ -14,27 +14,29 @@ size 3	size 6
 (You must solve this problem using only ONE public static final constant, not multiple constants; and its value must be used in the way described in this problem.)
 
 SIZE 	/ and \			*
-6 		20,16,12,8,4 	0,8,16,24,32,40
+6 		20,16,12,8,4 	0,8,16,24,32,40 
 3 		8,4,0 			0,8,16
 
 size 6 = -4 * line + 24
 size 3 = -4 * line + 12
 expression (multiply by 4)
--4 * line + (4 * SIZe);
+-4 * line + (4 * SIZE) for slashes
+
+size 5 = 8 * line - 8
 
 */
 public class StarFigureTwo {
 	public static final int SIZE = 3;
 	public static void main(String[] args){
 
-		for(int line = 1; line <= 5; line++){
-			for(int i = 0; i < -4 * line + 4 * SIZE; i++){
+		for(int line = 1; line <= SIZE; line++){
+			for(int i = 0; i < (-4 * line + 4 * SIZE); i++){
 				System.out.print("/");
 			}
 			for(int j = 0; j < 8 * line - 8; j++){
 				System.out.print("*");
 			}
-			for(int i = 0; i < -4 * line + 4 * SIZE; i++){
+			for(int i = 0; i < (-4 * line + 4 * SIZE); i++){
 				System.out.print("\\");
 			}
 			System.out.println();
