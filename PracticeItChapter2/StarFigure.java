f3d93d8a4d220bfe12727d3e5a73c6a5c7cb4a1e
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-24 20:35:15 -0700

    finished with asterisks

---
+++ PracticeItChapter2/StarFigure.java
@@ -0,0 +1,23 @@
+/*
+Write a program in a class named StarFigure that produces the following output using for loops.
+
+////////////////\\\\\\\\\\\\\\\\
+////////////********\\\\\\\\\\\\
+////////****************\\\\\\\\
+////************************\\\\
+********************************
+*/
+public class StarFigure {
+	public static void main(String[] args){
+
+		for(int line = 1; line <= 5; line++){
+			for(int i = 0; i < -4 * line + 20; i++){
+				System.out.print("/");
+			}
+			for(int j = 0; j < 8 * line - 8; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
