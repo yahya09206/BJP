c8a46afef34d46b243cae005d9fe0168099f0eff
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-01 20:28:49 -0700

    done with exercise to print strings a certain amount of times

---
+++ PracticeItChapter3/PrintStrings.java
@@ -0,0 +1,19 @@
+/*
+Write a method called printStrings that accepts a String and a number of repetitions as parameters and prints that String the given number of times. For example, the call:
+
+printStrings("abc", 5);
+will print the following output:
+
+abcabcabcabcabc
+*/
+public class PrintStrings {
+	public static void main(String[] args){
+		printStrings("abc", 5);
+	}
+
+	public static void printStrings(String a, int num){
+		for(int i = 0; i <= num; i++){
+			System.out.println(a);
+		}
+	}
+}
