f3f10dd98363b65e312c67fec7c57861507bea02
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-01 20:50:46 -0700

    progrmt that finds the smallest of three numbers

---
+++ PracticeItChapter3/Min.java
@@ -0,0 +1,12 @@
+/*
+Write a method called min that takes three integers as parameters and returns the smallest of the three values, such that a call of min(3, -2, 7) would return -2, and a call of min(19, 27, 6) would return 6. Use Math.min to write your solution.
+*/
+public class Min {
+	public static void main(String[] args){
+		min(a,b,c);
+	}
+
+	public static void min(int a, int b, int c){
+		
+	}
+}
