efc69098ffa5ba2f4ac1286402f7f07bd6eaf2b8
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-02 20:33:33 -0700

    done with that

--- PracticeItChapter3/CylinderSurfaceArea.java
+++ PracticeItChapter3/CylinderSurfaceArea.java
@@ -9,5 +9,6 @@ public class CylinderSurfaceArea {
 		cylinderSurfaceArea(3.0, 4.5);
 	}
 
-	
-}
+	public double cylinderSurfaceArea(double r, double h){
+		return (2 * Math.PI * r * r) + (2 * Math.PI * r * h);
+	}}
