36adcb80265667b1bc987ee0e8f571b796cd1b33
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-02 19:07:12 -0700

    program to repeat phrase a specific amount of times

---
+++ PracticeItChapter3/RobertPaulson.java
@@ -0,0 +1,16 @@
+/*
+Write code that prompts the user for a phrase and a number of times to repeat it, then prints the phrase that many times. Here is an example dialogue with the user:
+
+What is your phrase? His name is Robert Paulson
+How many times should I repeat it? 3
+His name is Robert Paulson
+His name is Robert Paulson
+His name is Robert Paulson
+*/
+import java.util.Scanner;
+public class RobertPaulson {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
