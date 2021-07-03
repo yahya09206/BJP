4dff4735b23558c06a100202761a5ca3e52fed4e
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-02 20:53:20 -0700

    print out reversed phrase

--- PracticeItChapter3/PrintReverse.java
+++ PracticeItChapter3/PrintReverse.java
@@ -4,3 +4,17 @@ Write a method called printReverse that accepts a String as its parameter and pr
 !ereht olleh
 If the empty string is passed, no output is produced. Your method should produce a complete line of output.
 */
+public class PrintReverse {
+	public static void main(String[] args){
+		printReverse("hello there!");
+	}
+
+	public static void printReverse(String phrase){
+		if(phrase.length() == 0){
+			System.out.print(" ");
+		}
+		for(int i = phrase.length() - 1; i >= 0; i--){
+			System.out.print(phrase.charAt(i));
+		}
+	}
+}
