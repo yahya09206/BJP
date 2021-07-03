d1d7feed650855077b0034d18d82c3f91d28d33d
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-02 19:39:41 -0700

    forgot to add return type for method

--- PracticeItChapter3/LargestAbsValue.java
+++
@@ -1,9 +0,0 @@
-/*
-Write a method called largerAbsVal that takes two integers as parameters and returns the larger of the two absolute values. A call of largerAbsVal(11, 2) would return 11, and a call of largerAbsVal(4, -5) would return 5.
-*/
-public class LargestAbsValue {
-	public static void main(String[] args){
-		largerAbsVal(11, 2);
-		largerAbsVal(4, -5);
-	}
-}
