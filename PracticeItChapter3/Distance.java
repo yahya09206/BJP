e8a8fe24f705edbca9223318c8475a7376d0c5cc
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-02 20:06:56 -0700

    method to find distance

---
+++ PracticeItChapter3/Distance.java
@@ -0,0 +1,13 @@
+/*
+Write a method called distance that accepts four integer coordinates x1, y1, x2, and y2 as parameters and computes the distance between points (x1, y1) and (x2, y2) on the Cartesian plane. The equation for the distance is:
+
+distance equation
+For example, the call of distance(1, 0, 4, 4) would return 5.0 and the call of distance(10, 2, 3, 5) would return 7.615773105863909.
+*/
+public class Distance {
+	public static void main(String[] args){
+		distance(1, 0, 4, 4);
+		distance(10, 2, 3, 5);
+	}
+	
+}
