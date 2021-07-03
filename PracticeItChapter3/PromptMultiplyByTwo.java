69651a5ad155a9b98bcdfbb7753b8b4be6df9efd
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-02 19:02:21 -0700

    prompt user for integer

---
+++ PracticeItChapter3/PromptMultiplyByTwo.java
@@ -0,0 +1,14 @@
+/*
+Write code to read an integer from the user, then print that number multiplied by 2. You may assume that the user types a valid integer. A sample run of the code would produce the following:
+
+Type an integer: 4
+4 times 2 = 8
+*/
+import java.util.Scanner;
+public class PromptMultiplyByTwo {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Type an integer: ");
+	}	
+}
