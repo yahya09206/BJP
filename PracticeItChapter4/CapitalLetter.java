12c1ef7838f16528acf872018447cd85daa574b6
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-04 14:16:05 -0700

    program to check if word starts with a capital letter

--- PracticeItChapter4/CapitalLetter.java
+++ PracticeItChapter4/CapitalLetter.java
@@ -1,13 +1,3 @@
 /*
 Write an if statement that tests to see whether a String stored in a variable named phrase begins with a capital letter. If the string begins with a capital letter, print capital . If not, print not capital . Use at least one method call from the Character class in your solution. You may assume that the string is non-empty (contains at least one character).
 */
-public class CapitalLetter {
-	public static void main(String[] args){
-		String phrase = "John";
-		if(Character.isUpperCase(phrase.charAt(0))){
-			System.out.print("capital");
-		}else {
-			System.out.print("not capital");
-		}
-	}
-}
