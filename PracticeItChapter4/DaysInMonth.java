d3f67560e1fe632c62ccbf23d0d55ca95c6e08ab
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-04 15:01:02 -0700

    set up conditions to check

--- PracticeItChapter4/DaysInMonth.java
+++ PracticeItChapter4/DaysInMonth.java
@@ -9,4 +9,13 @@ public class DaysInMonth {
 		System.out.print(daysInMonth(9));
 	}
 	
+	public static int daysInMonth(int month){
+		if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+			return 31;
+		}else if(month == 2){
+			return 28;
+		}else {
+			return 30;
+		}
+	}
 }
