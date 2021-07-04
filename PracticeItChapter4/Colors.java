4f89825ddf563af3e6c7b5dde984eb7becb0b288
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-03 20:48:33 -0700

    fixed statements

--- PracticeItChapter4/Colors.java
+++ PracticeItChapter4/Colors.java
@@ -14,11 +14,11 @@ public class Colors {
 		String color = scan.next();
 
 		if(color.equalsIgnoreCase("R")){
-			System.out.println("You have chosen " + color + ".");
+			System.out.println("You have chosen Red.");
 		}else if(color.equalsIgnoreCase("B")){
-			System.out.println("You have chosen " + color + ".");
+			System.out.println("You have chosen Blue.");
 		}else if(color.equalsIgnoreCase("G")){
-			System.out.println("You have chosen " + color + ".");
+			System.out.println("You have chosen Green.");
 		}else {
 			System.out.println("Unknown color: " + color);
 		}
