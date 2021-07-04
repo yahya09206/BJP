12758ae5d85f003e056929d4b0ce4c786810e980
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-04 14:19:49 -0700

    mixed up where the methods go

--- PracticeItChapter4/CapitalLetter.java
+++ PracticeItChapter4/CapitalLetter.java
@@ -1,3 +1,12 @@
 /*
 Write an if statement that tests to see whether a String stored in a variable named phrase begins with a capital letter. If the string begins with a capital letter, print capital . If not, print not capital . Use at least one method call from the Character class in your solution. You may assume that the string is non-empty (contains at least one character).
 */
+public class CapitalLetter {
+	public static void main(String[] args){
+		if(Character.isUpperCase(phrase.charAt(0))){
+			System.out.println("capital");
+		}else{
+			System.out.println("not capital");
+		}
+	}
+}
