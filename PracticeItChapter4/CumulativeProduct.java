44da3eed1bdf07ea1882473d8c0ca62bed6bed39
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-03 20:55:55 -0700

    prompt user for input

--- PracticeItChapter4/CumulativeProduct.java
+++ PracticeItChapter4/CumulativeProduct.java
@@ -11,6 +11,9 @@ Product = 630
 import java.util.Scanner;
 public class CumulativeProduct {
 	public static void main(String[] args){
-		
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("How many numbers? ");
+		int num = scan.nextInt();
 	}
 }
