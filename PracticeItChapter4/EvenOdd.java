43d3c62fbadacc7433168298a3cbadfea2b341dc
Yahya Hussein <yahyahussein91@gmail.com>
2021-07-03 20:22:10 -0700

    condition to check whether even or odd

--- PracticeItChapter4/EvenOdd.java
+++ PracticeItChapter4/EvenOdd.java
@@ -10,5 +10,11 @@ public class EvenOdd {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter a number: ");
 		int num = scan.nextInt();
+
+		if(num % 2 == 0){
+			System.out.println("even");
+		}else {
+			System.out.println("odd");
+		}
 	}
 }
